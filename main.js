bb3476f455df75223aef821861b4928fbee4333b
makbarmaulana011 <makbarmaulanam@gmail.com>
2023-10-12 16:38:05 +0700

    update belajarJS

---
+++ main.js
@@ -0,0 +1,45 @@
+// console.log("Hello World")
+// alert("Notifikasi")
+// prompt("David Da Silva Gacor")
+
+// var Promnet = "Coding Is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding Not Easy"
+// console.log(Promnet)
+
+// let Promnet = "Coding Is Easy"
+// console.log(Promnet)
+// Promnet = "Coding Is Easy"
+// console.log(Promnet)
+
+// const Promnet = "Coding Is Easy"
+// console.log(Promnet)
+// Promnet = "Coding Not Easy"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukan Poin Anda")
+//    if(totalPoin > 100){
+//    document.write("<h1>Congratulation</h1>")
+//    }
+//    else{
+//     document.write("<h1>Thank You</h1>")
+//    }
+
+//    let x = 6;
+//    let y = 3;
+
+//    console.log((x<10 && y > 1))
+//    console.log((x<10 && y < 1))
+//    console.log((x==5||y==5)) 
+//    console.log((x==6||y==5)) 
+//    console.log(!(x==y))
+
+  // let p = document.querySelector("p")
+  // let button = document.querySelector("button")
+  // let input = document.querySelector("input")
+
+  // button.addEventListener('click', function(){
+  //     let isi = input.value
+  //     console.log(isi)
+  //     p.innerText = isi
+  // })
